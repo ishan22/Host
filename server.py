bd65f3cb4b0f71e6b293d829e25419be057a52bf
Ishan Mitra <ishanbubby@gmail.com>
2019-10-12 18:54:28 -0700

    Add skeleton code

--- server.py
+++ server.py
@@ -2,15 +2,19 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# BACKEND/HOST
+
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
-
 @app.route('/getPid')
-def get_pid():
-    return 'PID'
-
+def get_pid(auth_token):
+    if !auth_token:
+        return err (403 authorization err)
+    if pid:
+        return pid
+    return create_pid()
 
 @app.route('/sortQueue')
 def sort():
@@ -20,10 +24,21 @@ def sort():
 def getQueue():
     return 'queue'
 
+@app.route('/getNextSong')
+def getNextSong():
+    return 'song'
+
+def create_pid():
+    # create pid
+    # return
+
+
+# USER
+
 @app.route('/search')
 def search():
     return 'searched'
 
-@app.route('/getNextSong')
-def getNextSong():
-    return 'song'
+def join_party():
+
+
