cb76f80566034ef8319f8887b86bd2aab76e7891
Ishan Mitra <ishanbubby@gmail.com>
2019-10-13 08:30:42 -0700

    Add to relabeling

--- app.py
+++ app.py
@@ -3,23 +3,23 @@ import os
 import io
 import re
 
-from base64 import standard_b64decode
+from base64 import b64decode
 from flask import Flask, url_for, request, redirect
 from werkzeug.utils import secure_filename
 
 UPLOAD_FOLDER = './'
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def hello_world():
     return 'welcome to <strong>seamless.</strong>'
 
-@app.route('/scan', methods=['POST'])
+@app.route('/scan', methods=['POST', 'GET'])
 def scan():
     data = request.json
     print(data)
-    image = io.BytesIO(standard_b64decode(data["data"])) #re.sub("data:image/jpeg;base64", '', data["data"])))
+    image = io.BytesIO(b64decode(data["data"])) #re.sub("data:image/jpeg;base64", '', data["data"])))
+    imgs = Image.open(image)
     ret = handler.get_data(image)
     return ret
