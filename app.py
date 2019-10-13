52e78fa2dc9738968f86704927186e99468ca3e1
Ishan Mitra <ishanbubby@gmail.com>
2019-10-13 08:07:39 -0700

    Add to server decoding

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 import io
 import re
 
-from base64 import b64decode
+from base64 import standard_b64decode
 from flask import Flask, url_for, request, redirect
 from werkzeug.utils import secure_filename
 
@@ -20,6 +20,6 @@ def hello_world():
 def scan():
     data = request.json
     print(data)
-    image = io.BytesIO(b64decode(re.sub("data:image/png;base64", '', data["data"])))
+    image = io.BytesIO(standard_b64decode(data["data"])) #re.sub("data:image/jpeg;base64", '', data["data"])))
     ret = handler.get_data(image)
     return ret
