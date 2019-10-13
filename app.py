d5feb8e2bc1fa3ab92d62df6d3bd5245b8286251
Ishan Mitra <ishanbubby@gmail.com>
2019-10-13 07:15:06 -0700

    App changes

--- app.py
+++ app.py
@@ -21,23 +21,5 @@ def scan():
     data = request.json
     print(data)
     image = io.BytesIO(b64decode(re.sub("data:image/png;base64", '', data["data"])))
-    print(image)
-    #with open("image.png", "wb") as fh:
-     #   fh.write(base64.decodebytes(bytes(data["data"])))
-    return "success"
-
-@app.route('/scan2', methods=['POST'])
-def scan_image():
-    print(request.files)
-    if 'file' not in request.files:
-        return 'FILE ERR'
-    imgFile = request.files['file']
-    filename = secure_filename(imgFile.filename)
-    print("Uploaded file:%s"%filename)
-    imgFile.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    with open(filename, 'r') as f:
-        imgData = f.getvalue()
-    #imgData = imgFile.getvalue()
-    print(imgData)
-    details = handler.get_details(imgData)
-    return redirect(url_for('upload_file', filename=filename))
+    ret = handler.handle(image)
+    return ret
