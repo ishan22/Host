ea0424fc381262c498bd9f3f3140004a0df8d5ea
Ishan Mitra <ishanbubby@gmail.com>
2019-10-12 23:31:52 -0700

    Remove old files

--- server.py
+++ server.py
@@ -8,37 +8,6 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
-@app.route('/getPid')
-def get_pid(auth_token):
-    if !auth_token:
-        return err (403 authorization err)
-    if pid:
-        return pid
-    return create_pid()
-
-@app.route('/sortQueue')
-def sort():
-    return 'SORTED'
-
-@app.route('/getQueue')
-def getQueue():
-    return 'queue'
-
-@app.route('/getNextSong')
-def getNextSong():
-    return 'song'
-
-def create_pid():
-    # create pid
-    # return
-
-
-# USER
-
-@app.route('/search')
-def search():
-    return 'searched'
-
-def join_party():
-
+@app.route('/add')
+def add_event():
 
