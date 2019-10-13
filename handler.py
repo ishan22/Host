e230784464dbe1162119f523b4a3d41abfc03069
fareedmabrouk <fareed72@uw.edu>
2019-10-13 05:29:21 -0700

    made a lot of changes that made it work mostly

--- handler.py
+++ handler.py
@@ -2,37 +2,56 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sat Oct 12 23:06:45 2019
-
 @author: FareedMabrouk
 """
+from azure.cognitiveservices.language.textanalytics import TextAnalyticsClient
+from msrest.authentication import CognitiveServicesCredentials
 from azure.cognitiveservices.vision.computervision import ComputerVisionClient
 from azure.cognitiveservices.vision.computervision.models import TextOperationStatusCodes
 from azure.cognitiveservices.vision.computervision.models import TextRecognitionMode
 from azure.cognitiveservices.vision.computervision.models import VisualFeatureTypes
 from msrest.authentication import CognitiveServicesCredentials
-
+import dateparser as dp
+import datetime
 import os
 import sys
 import time
 
 os.environ["COMPUTER_VISION_SUBSCRIPTION_KEY"] = 'a6f152d25bd04b25a831b7a17fdfa594'
 os.environ["COMPUTER_VISION_ENDPOINT"] = 'https://seamlessevents.cognitiveservices.azure.com/'
-# Add your Computer Vision subscription key to your environment variables.
+os.environ["TEXT_ANALYTICS_SUBSCRIPTION_KEY"] = '0122840605a3487c9b6a385ee25148db'
+os.environ["TEXT_ANALYTICS_ENDPOINT"] = 'https://seamlesstextanalytics.cognitiveservices.azure.com/'
+
+key_var_name = 'TEXT_ANALYTICS_SUBSCRIPTION_KEY'
+if not key_var_name in os.environ:
+    raise Exception('Please set/export the environment variable: {}'.format(key_var_name))
+subscription_key = os.environ[key_var_name]
+
+endpoint_var_name = 'TEXT_ANALYTICS_ENDPOINT'
+if not endpoint_var_name in os.environ:
+    raise Exception('Please set/export the environment variable: {}'.format(endpoint_var_name))
+endpoint = os.environ[endpoint_var_name]
+
+credentials = CognitiveServicesCredentials(subscription_key)
+
+
+
 
 if 'COMPUTER_VISION_SUBSCRIPTION_KEY' in os.environ:
-    subscription_key = os.environ['COMPUTER_VISION_SUBSCRIPTION_KEY']
+    subscription_key_cv = os.environ['COMPUTER_VISION_SUBSCRIPTION_KEY']
 else:
     print("\nSet the COMPUTER_VISION_SUBSCRIPTION_KEY environment variable.\n**Restart your shell or IDE for changes to take effect.**")
     sys.exit()
 # Add your Computer Vision endpoint to your environment variables.
 if 'COMPUTER_VISION_ENDPOINT' in os.environ:
-    endpoint = os.environ['COMPUTER_VISION_ENDPOINT']
+    endpoint_cv = os.environ['COMPUTER_VISION_ENDPOINT']
 else:
     print("\nSet the COMPUTER_VISION_ENDPOINT environment variable.\n**Restart your shell or IDE for changes to take effect.**")
     sys.exit()
-computervision_client = ComputerVisionClient(endpoint, CognitiveServicesCredentials(subscription_key))
-DIR = '/Users/FareedMabrouk/Desktop/Explore/Coding/DubHacks/Seamless Events/Images/'
-remote_image_printed_text_url = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJJAExwQSQED32mmcvbHY0nXz3mJsN7a8gUVMaarpEc-xiAn_a'
+computervision_client = ComputerVisionClient(endpoint_cv, CognitiveServicesCredentials(subscription_key_cv))
+text_analytics = TextAnalyticsClient(endpoint, credentials=credentials)
+dir = 'https://marketplace.canva.com/MADOPir-esE/1/0/thumbnail_large-1/canva-blue-green-red-music-icons-open-mic-night-flyer-MADOPir-esE.jpg'
+remote_image_printed_text_url = dir
 recognize_printed_results = computervision_client.batch_read_file(url=remote_image_printed_text_url,  raw=True)
 
 
@@ -41,18 +60,72 @@ operation_location_remote = recognize_printed_results.headers["Operation-Locatio
 # Grab the ID from the URL
 operation_id = operation_location_remote.split("/")[-1]
 
-# Call the "GET" API and wait for it to retrieve the results 
+# Call the "GET" API and wait for it to retrieve the results
 while True:
     get_printed_text_results = computervision_client.get_read_operation_result(operation_id)
     if get_printed_text_results.status not in ['NotStarted', 'Running']:
         break
     time.sleep(1)
 
+full_text = ""
 # Print the detected text, line by line
 if get_printed_text_results.status == TextOperationStatusCodes.succeeded:
     for text_result in get_printed_text_results.recognition_results:
         for line in text_result.lines:
-            print(line.text)
-            print(line.bounding_box)
+            full_text += line.text + '\n'
 
-print()
+documents = [
+    {
+        "id": "1",
+        "language": "en",
+        "text": full_text
+    }
+]
+
+e_details = {
+    "title": '',
+    "date": '',
+    "start_time": '',
+    "end_time: ''"
+}
+
+title = ""
+big_area = 0
+threshold = 10
+for text_result in get_printed_text_results.recognition_results:
+    for line in text_result.lines:
+        box = line.bounding_box
+        curr_area = box[5] - box[1]
+        if curr_area > big_area + threshold:
+            title = line.text
+            big_area = curr_area
+        elif curr_area >= big_area - threshold and curr_area <= big_area + threshold:
+            title += " " + line.text
+
+e_details['title'] = title
+response = text_analytics.entities(documents=documents)
+for document in response.documents:
+    for entity in document.entities:
+        if entity.sub_type == 'TimeRange':
+            e_details['time'] = entity.name
+        if entity.sub_type = 'DateTimeRange':
+            e_details
+        if entity.type == 'DateTime':
+            e_details['date'] += ' ' + entity.name
+for document in response.documents:
+    print("Document Id: ", document.id)
+    print("\tKey Entities:")
+    for entity in document.entities:
+        print("\t\t", "NAME: ", entity.name, "\tType: ",
+              entity.type, "\tSub-type: ", entity.sub_type)
+        for match in entity.matches:
+            print("\t\t\tOffset: ", match.offset, "\tLength: ", match.length, "\tScore: ",
+                  "{:.2f}".format(match.entity_type_score))
+#print(e_details['date'])
+#new_date = dp.parse(e_details['date'], settings={'PREFER_DATES_FROM': 'future'})
+#print(new_date)
+#e_details['date'] = dp.parse(e_details['date']).datetime()
+if not dp.parse(e_details['date']) == None:
+    print('converted the date')
+    e_details['date'] = dp.parse(e.details['date'])
+print(e_details)
